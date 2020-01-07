f46e748e3f45c9081269d37e147e4b94b12b78b6
aarjas <56072240+aarjas@users.noreply.github.com>
2020-01-07 15:27:35 +0200

    Update instructions_jointdynher.R

--- instructions_jointdynher.R
+++ instructions_jointdynher.R
@@ -7,8 +7,8 @@ library(RcppEigen)
 
 Sys.setenv("PKG_CXXFLAGS"="-fopenmp -std=c++11 -O3 -DEIGEN_NO_DEBUG")
 Sys.setenv("PKG_LIBS"="-fopenmp") 					#Compiler flags
-setwd("C:/Users/Arttu/OneDrive/codes") 					#Sets working directory to where the code is
-									#Change this to the right one
+setwd("C:/Users/Arttu/Documents/GitHub/aarjas.github.io")	#Sets working directory to where the code is
+										#Change this to the right one
 sourceCpp("jointdynher.cpp") 						#Compiles the code
 
 #Create some data
@@ -28,9 +28,9 @@ for(i in 1:t)
 	y[,i]=(U%*%diag(sqrt(lambda))%*%t(U))%*%rnorm(n)
 }
 
-Nsim=10000								#Sets the number of MCMC-iterations
-linit=t/3 								#Initial values for the length scales. t/3 is usually fine
-cores=parallel::detectCores() 						#Number of processor cores
+Nsim=10000									#Sets the number of MCMC-iterations
+linit=t/3 									#Initial values for the length scales. t/3 is usually fine
+cores=parallel::detectCores() 					#Number of processor cores
 
 #The simulation function takes 6 parameters: the data, number of iterations, initial values for both length
 #scales, the eigenvectors and eigenvalues of the relationship matrix and the number of processor cores.
@@ -42,12 +42,12 @@ C=sim(y,Nsim,linit,linit,U,ksi,cores/2)
 #Examine the results
 meanse=colMeans(C$ser)							#Environmental variance posterior mean
 meansg=colMeans(C$sgr)							#Genetic variance posterior mean
-boundsse=apply(C$ser,2,quantile,c(0.025,0.975))				#95% credible interval of env. variance
-boundssg=apply(C$sgr,2,quantile,c(0.025,0.975)) 			#95% credible interval of gen. variance
+boundsse=apply(C$ser,2,quantile,c(0.025,0.975))			#95% credible interval of env. variance
+boundssg=apply(C$sgr,2,quantile,c(0.025,0.975)) 		#95% credible interval of gen. variance
 her=C$sgr/(C$sgr+C$ser) 						#Compute heritability
 meanher=colMeans(her)							#Posterior mean of heritability
-boundsher=apply(her,2,quantile,c(0.025,0.975)) 				#95% credible interval of heritability
-paste(round(C$Time_loop/3600*60,1), "minutes")				#Computation time
+boundsher=apply(her,2,quantile,c(0.025,0.975)) 			#95% credible interval of heritability
+paste(round(C$Time_loop/3600*60,1), "minutes")			#Computation time
 
 #Plotting
 par(mfrow=c(3,1))
